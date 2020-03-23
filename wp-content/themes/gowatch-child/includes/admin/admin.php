f761f128899210a76670924ba1c508f33de6b2a3
Ciorici Stefan <stefan@touchsize.com>
2020-03-23 14:02:38 +0200

    Styles the Processing State Tab from the Admin panel, created a toggled list instead and changed the refresh option

--- wp-content/themes/gowatch-child/includes/admin/admin.php
+++ wp-content/themes/gowatch-child/includes/admin/admin.php
@@ -102,10 +102,83 @@ class CONSTRUKTED_Admin {
         $js_path = '/wp-content/themes/gowatch-child/includes/admin/js/construkted.js';
 
         wp_enqueue_script( 'construkted-admin-script', $js_path, array('jquery'), false, true );
+        wp_enqueue_style('construkted-admin-css', get_stylesheet_directory_uri() . '/includes/admin/css/gw3-admin-css.css' );
 
         wp_localize_script( 'construkted-admin-script', 'construktedAdminParam', array(
-                'tilingStateEndPoint'       => CONSTRUKTED_EC2_API_TASK_ALL
+                'tilingStateEndPoint'       => admin_url("admin-ajax.php")
             )
         );
     }
 }
+
+
+
+// Create the AJAX functions for the processing state
+
+function gw3_processing_retrieve_data() {
+    $url = CONSTRUKTED_EC2_API_TASK_ALL;
+    $ret = wp_remote_get( $url );
+
+    // check error
+    if(is_wp_error($ret)) {
+        $body = "something wrong. failed to connect construkted API";
+    }
+    else {
+        $body = json_decode($ret['body'], true);
+    }
+
+    return $body;
+}
+
+function gw3_processing_generateItem($item) {
+
+    ob_start();
+    ob_clean();
+
+    ?>
+    
+    <div class="processing-item">
+        
+        <div class="item-title"><?php echo $item['name']; ?></div>
+
+        <div class="item-content">
+            <?php foreach( $item as $key => $attr ): ?>
+                <div class="flex-item">
+                    <div>
+                        <?php echo $key; ?>: 
+                    </div>
+                    <div>
+                        <?php echo is_array($attr) ? $attr['code'] : $attr; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <span class="dashicons dashicons-arrow-down-alt2"></span>
+
+    </div>
+
+    <?php
+
+    $output = ob_get_clean();
+
+    return $output;
+
+}
+
+function gw3_processing_displayItems() {
+
+    $items  = gw3_processing_retrieve_data();
+    $output = '';
+
+    foreach ($items as $item) {
+        $output .= gw3_processing_generateItem($item);
+    }
+
+    if( isset($_POST['echo']) ) {
+        echo $output;
+        die();
+    }
+
+    return $output;
+}
+add_action( 'wp_ajax_gw3_processing_displayItems', 'gw3_processing_displayItems');
