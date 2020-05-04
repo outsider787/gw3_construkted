6d15905fbdcb68ba30fdb4031dded7c050455a92
Adrian <aonsen@gmail.com>
2020-05-04 14:59:22 -0400

    Update settings-popup.php

--- wp-content/themes/gowatch-child/includes/construkted/side-bar/settings-popup.php
+++ wp-content/themes/gowatch-child/includes/construkted/side-bar/settings-popup.php
@@ -4,12 +4,19 @@
 
         <div class="content-wrapper">
             <div class="form-group">
-                <label for="display_performance">Display performance</label>
+                <label for="display_performance"><strong>Display</strong></label>
+                <br>Rendering performance
                 <input type="range" id="maximum-screen-space-error-slider" class="form-control-range" name="display_performance" min="2" max="32" value="24">
                 <div class="label-min-max">
                     <span class="lbl-min">Performance</span>
                     <span class="lbl-max" style="float:right">Quality</span>
                 </div>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" id="show-hide-all-measurement-checkbox" type="checkbox">
+                <label class="form-check-label" for="show-hide-all-measurement-checkbox">
+                    Show Wireframe
+                </label>
             </div>
 
             <!-- These buttons should only be visible to the asset owner (logged in) -->
@@ -18,6 +25,8 @@
             global $post;
 
             if($post->post_author == get_current_user_id()) { ?>
+                <hr style="width:100%;text-align:left;margin-left:0">
+                <strong>Thumbnail and View Settings</strong>    
                 <p class="desc">Create/update thumbnail from current asset view.</p>
                 <button type="button" id="capture_thumbnail" class="btn btn-light">Update asset thumbnail</button>
 
@@ -28,7 +37,7 @@
                 <button type="button" id="reset_camera_view" class="btn btn-light">Camera view reset</button>
             <?php } ?>
 
-            <div class="form-group">
+            <div class="form-group hidden">
                 <label for="fpv_speed">FPV movement speed</label>
                 <input type="range" class="form-control-range" name="fpv_speed" id="fpv_speed">
                 <div class="label-min-max">
