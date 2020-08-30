f5acd002f82da49da606dce887bd80b2051f983f
3dgisking <wugis1219@gmail.com>
2020-08-31 00:57:07 +0800

    https://github.com/Construkted-Reality/construkted_api/issues/23 Count polygons and verticies #23

--- wp-content/themes/gowatch-child/control_asset_details.php
+++ wp-content/themes/gowatch-child/control_asset_details.php
@@ -11,13 +11,6 @@ header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 header('Access-Control-Allow-Headers: Content-Type');
 
-/*
-    localhost  '::1'
-    https://tile01.construkted.com '158.106.136.192'
-    https://tile02.construkted.com '135.23.228.38'
-    myLocal '185.164.35.17'
-*/
-
 $allowedIPs = [
     '::1',             // localhost
     '158.106.136.192', // tile.api.construkted.com or tile-gw3.construkted.com
@@ -40,7 +33,6 @@ if(!isset($_SERVER['REMOTE_ADDR']) || (isset($_SERVER['REMOTE_ADDR']) && !in_arr
  * Function which sets the default thumbnail with the "no-thumbnail" preset
  */
 function set_default_thumbnail_of_no_thumbnail($post_id, $slug) {
-
     // Check if we have the ID of the thumbnail stored in the object cache
     $noimage_thumbnail_id = wp_cache_get('construkted_noimage', 'construkted');
 
@@ -55,10 +47,7 @@ function set_default_thumbnail_of_no_thumbnail($post_id, $slug) {
         if( $default_image ) {
             $default_image = wp_get_attachment_by_post_name('default_nothumbnail_image');
             $attachment_id = $default_image;
-
         } else {
-
-
             // If nothing is found in object cache and no media ID with that title if found, upload a new one
             $thumbnailFileName = 'thumbnail_' . $slug . '.png';
             $wordpress_upload_dir = wp_upload_dir();
@@ -99,11 +88,9 @@ function set_default_thumbnail_of_no_thumbnail($post_id, $slug) {
             $attachment_meta_data = wp_generate_attachment_metadata( $attachment_id, $thumbnail_file_path);
 
             wp_update_attachment_metadata($attachment_id, $attachment_meta_data);
-
         }
         wp_cache_set('construkted_noimage', $attachment_id, 'construkted');
     }
-    
 
     $ret = update_post_meta( $post_id, '_thumbnail_id', $attachment_id );
 
@@ -111,17 +98,12 @@ function set_default_thumbnail_of_no_thumbnail($post_id, $slug) {
         wp_die('failed to update post meta!');
 }
 
-
 if( 'delete_attachment' == $_REQUEST['action'] ) {
-
-
 	// Do whatever necessary for the delete_attachment action
 	if(isset($_REQUEST['post_id']) && !empty($_REQUEST['post_id'])) {
-
 	    // Actually load the WP environment only if all tests are passed and WP is needed
 	    require( '../../../wp-load.php' );
 		require_once ABSPATH . '/wp-admin/includes/image.php';
-
 
 	    $post_id = esc_attr($_REQUEST['post_id']);
 	    $attachment_id = esc_attr($_REQUEST['orig_asset_attachment_id']);
@@ -137,8 +119,6 @@ if( 'delete_attachment' == $_REQUEST['action'] ) {
 	    }
 
 	    $attached_file = get_attached_file($attachment_id, false);
-
-
 
 	    $ret = wp_delete_attachment( $attachment_id, true );
 
@@ -156,11 +136,8 @@ if( 'delete_attachment' == $_REQUEST['action'] ) {
 	    exit;
 	}
 } elseif( 'publish_asset' == $_REQUEST['action'] ) {
-
 	// Do whatever needs to be done to publish an asset
 	if( isset($_REQUEST['post_id']) && !empty($_REQUEST['post_id']) ) {
-
-
 	    require( '../../../wp-load.php' );
 		require_once ABSPATH . '/wp-admin/includes/image.php';
 		
@@ -182,11 +159,23 @@ if( 'delete_attachment' == $_REQUEST['action'] ) {
 	    // check old thumbnail and delete
 	    if($thumbnail_id != "")
 	        if( ! wp_delete_attachment( $thumbnail_id, true )) {
-	            echo json_encode(array('errCode' => 0, 'errMsg' => 'failed to delete old thumbnail!'));
+	            echo json_encode(array('errCode' => 1, 'errMsg' => 'failed to delete old thumbnail!'));
 	            exit;
 	        }
 
 	    set_default_thumbnail_of_no_thumbnail($post_id, $slug);
+
+        $mesh_polygon_count = 0;
+
+        if(isset($_REQUEST['count_of_triangles']))
+            $mesh_polygon_count = $_REQUEST['count_of_triangles'];
+
+        $ret = update_post_meta( $post_id, 'mesh_polygon_count', $mesh_polygon_count);
+
+        if($ret == false){
+            echo json_encode(array('errCode' => 1, 'errMsg' => 'failed to update mesh polygon count meta field!'));
+            exit;
+        }
 
 	    echo json_encode(array('errCode' => 0, 'errMsg' => 'successfully published!'));
 	    exit;
@@ -195,5 +184,4 @@ if( 'delete_attachment' == $_REQUEST['action'] ) {
 	    echo json_encode(array('errCode' => 1, 'errMsg' => 'please specify post id!'));
 	    exit;
 	}
-
 }
