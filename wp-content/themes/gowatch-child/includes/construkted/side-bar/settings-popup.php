5dc651c7a13a148b9dc10c5804e0d69c564ac74c
Adrian <aonsen@gmail.com>
2021-02-21 16:10:52 -0500

    Update settings-popup.php

--- wp-content/themes/gowatch-child/includes/construkted/side-bar/settings-popup.php
+++ wp-content/themes/gowatch-child/includes/construkted/side-bar/settings-popup.php
@@ -22,7 +22,7 @@
              <div class="form-group">
                 <label for="fpv_speed"><strong>FPV Movement</strong></label>
                 <br>Movement Speed 
-                <input type="range" id="fpv-movement-speed-slider" class="form-control-range" name="fpv_speed" min="0.01" max="1" value="0.4" step="0.01">
+                <input type="range" id="fpv-movement-speed-slider" class="form-control-range" name="fpv_speed" min="0.01" max="2" value="0.4" step="0.01">
                 <div class="label-min-max">
                     <span class="lbl-min">Slow</span>
                     <span class="lbl-max" style="float:right">Fast</span>
